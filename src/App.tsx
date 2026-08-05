2bcc861e642b4c2e5aff61698574b7fd4bdddfd9
Rohan Rahim <rohanrahim024@gmail.com>
2026-08-05 22:48:39 +0600

    Project Initialize with react(tsx) and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-dvh flex items-center justify-center bg-[#FFE5B4]">
+      <p className="text-2xl font-bold">Welcome to ThermalPaste</p>
+    </div>
+  );
+}
+
+export default App;
