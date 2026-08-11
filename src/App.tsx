cde0c2191a2b592a34cf8284e887a2f4cc21a745
Rohan Rahim <rohanrahim024@gmail.com>
2026-08-11 20:16:40 +0600

    feat(component): add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="h-dvh flex items-center justify-center bg-[#FFE5B4]">
-      <p className="text-2xl font-bold">Welcome to ThermalPaste</p>
+    <div className="">
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import { Plus, Search } from "lucide-react";
+
+function Navbar() {
+  return (
+    <header className="w-full bg-[#0F1117] border-b border-[#222834] px-8 py-2.5 flex items-center justify-between gap-4 select-none sticky top-0 z-50">
+      {/* LOGO */}
+      <div className="flex items-center gap-3 shrink-0">
+        <p className="font-bold text-white text-2xl">ThermalPaste</p>
+      </div>
+
+      {/* SEARCH BAR */}
+      <div className="flex-1 max-w-xl relative">
+        <div className="relative flex items-center">
+          <Search className="w-4 h-4 text-[#8F99A8] absolute left-3.5 pointer-events-none" />
+          <input
+            type="text"
+            placeholder="Search posts, component specs (e.g., RTX 4090, AM5)..."
+            className="w-full bg-[#161922] hover:bg-[#1E2330] focus:bg-[#161922] text-xs text-white placeholder-[#8F99A8] pl-10 pr-24 py-2 rounded-full border border-[#222834] focus:border-[#00D8F6] focus:outline-none transition-all duration-200"
+          />
+        </div>
+      </div>
+
+      {/* ACTION ICONS & USER PROFILE*/}
+      <div className="flex items-center gap-x-5 sm:gap-2 shrink-0">
+        {/* CREATE POST */}
+        <button className="flex items-center gap-1.5 bg-[#00D8F6] hover:bg-[#00c4e0] text-[#0B0D11] text-xs font-bold px-3 py-1.5 rounded-full transition shadow-[0_0_12px_rgba(0,216,246,0.25)] active:scale-95 cursor-pointer">
+          <Plus className="w-4 h-4 stroke-[2.5]" />
+          <span className="hidden md:inline uppercase tracking-wider text-[11px]">
+            Create
+          </span>
+        </button>
+
+        {/* USER AVATAR */}
+        <button className="relative ml-1 p-0.5 rounded-full hover:ring-2 hover:ring-[#00D8F6]/50 transition">
+          <div className="w-8 h-8 rounded-full bg-emerald-900 border border-emerald-500/40 flex items-center justify-center text-xs font-bold text-emerald-200">
+            P
+          </div>
+          <span className="w-2.5 h-2.5 bg-emerald-400 border-2 border-[#0F1117] rounded-full absolute bottom-0 right-0"></span>
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
